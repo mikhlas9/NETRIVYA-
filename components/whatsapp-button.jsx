555cd73f2ba2f67579a00913dabf46dfde1b4a6d
mikhlas9 <mohammadikhlas99@gmail.com>
2026-01-19 14:40:29 +0530

    no. updated

--- components/whatsapp-button.jsx
+++ components/whatsapp-button.jsx
@@ -2,7 +2,7 @@ export default function WhatsAppButton() {
   return (
     <div className="fixed bottom-8 left-8 z-40">
       <a
-        href="https://wa.me/+919906610175"
+        href="https://wa.me/+919149527647"
         target="_blank"
         rel="noopener noreferrer"
         className="flex items-center justify-center w-16 h-16 bg-green-500 hover:bg-green-600 text-white rounded-full shadow-lg transition duration-200 transform hover:scale-110 overflow-hidden"
